fc437709d1c37e46cc640f3dfba6a7b0e46ed4f3
Nishant nayak <nayaknishant656@gmail.com>
2024-08-17 23:17:31 +0530

    hnn

--- src/components/body/geendu.js
+++ src/components/body/geendu.js
@@ -1,8 +1,13 @@
-
 import Skeleton from 'react-loading-skeleton'
 import 'react-loading-skeleton/dist/skeleton.css'
 import "./jamin.css"
-
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+// import { Pagination, Navigation } from 'swiper/modules';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import Image from "./images/duplicate.jpg";
 
 import { metronome } from 'ldrs'
 
@@ -13,12 +18,17 @@ metronome.register()
 
 const Table = ({ products }) => {
     // const firstProduct = products.length > 0 ? products[0] : null;
-    let firstProduct;
-    if (products.length > 0) {
-        const { _id, si, name, amount, address, year, text } = products[0];
-        firstProduct = { _id, si, name, amount, address, year, text };
+    let firstProduct = true;
+    // if (products.length > 0) {
+    //     const { _id, si, name, amount, address, year, text } = products[0];
+    //     firstProduct = { _id, si, name, amount, address, year, text };
+    // }
+    let imgwidth = {
+        width:"100px",
+        height: "100px",
+        margin: "0px 10px 10px 10px "
     }
-
+    console.log(products);
     let link = "https://nawnitstudio.000webhostapp.com/certificate/index.html";
     return (
         <>
@@ -40,8 +50,55 @@ const Table = ({ products }) => {
                                         <div className='header_body__child_jamin'>
                                             <h1>अब जामिन रसीद बनेगी आसान, आपके अपने ऑनलाइन सेवा केंद्र पर</h1>
                                         </div>
-                                        <p>{firstProduct.text ? firstProduct.text : <Skeleton count={10} />}</p><br></br>
-                                        <p>{firstProduct.text ? firstProduct.text : <Skeleton count={10} />}</p>
+                                        <p>{products.text}</p>
+                                        {/* <div className='swiper-grandparent'>
+                                            <div className='swiper-child-parent'>
+                                                <Swiper
+                                                    pagination={{ type: 'progressbar' }}
+                                                    navigation={true}
+                                                    modules={[Pagination, Navigation]}
+                                                    className="mySwiper"
+                                                >
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                    <SwiperSlide><img src={Image}></img></SwiperSlide>
+                                                </Swiper>
+                                            </div>
+                                        </div> */}
+                                        {/* <Swiper
+                                            // install Swiper modules
+                                            modules={[Navigation, Pagination, A11y]}
+                                            spaceBetween={10}
+                                            slidesPerView={1}
+                                            navigation
+                                            pagination={{ clickable: true }}
+                                            onSwiper={(swiper) => console.log(swiper)}
+                                            onSlideChange={() => console.log('slide change')}
+                                        >
+                                            {products.images?.map((mylist2) => (
+                                                <SwiperSlide>
+                                                    <img style={imgwidth} src={mylist2} />
+                                                </SwiperSlide>
+                                            ))};
+                                        </Swiper> */}
+                                        {products.images?.map((mylist2) => (
+                                                
+                                                    <img style={imgwidth} src={mylist2} />
+                                              
+                                            ))};
+
+                                        {products.points?.map((mylist2) => (
+                                            <ul><li>{mylist2}</li></ul>
+                                        ))};
+                                        {/* <p>{firstProduct.text ? firstProduct.text : <Skeleton count={10} />}</p><br></br>
+                                        <p>{firstProduct.text ? firstProduct.text : <Skeleton count={10} />}</p> */}
+                                       
                                         <p><a href={link}>recipt-link</a></p>
                                     </div>
                                 </div>
