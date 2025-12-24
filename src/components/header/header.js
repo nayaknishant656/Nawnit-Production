f4fa00267d3bcdf14152b55fd834be4401157721
nayaknishant656 <nayaknishant656@gmail.com>
2025-12-24 23:16:45 +0530

    header file changed

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,13 +9,13 @@ export default function header() {
             <div className='nav_main__parent'>
                 <div className='nav__right'>
                     <h3> समाधानी - AI Portal </h3>
-                    <details>
+                    {/* <details>
                         <summary>
                         <p className='summary_p'>समाधानी एक ऑनलाइन सेवा केंद्र (वेब पोर्टल) है जो सभी प्रकार की सरकारी सेवाओं और योजनाओं को लोगों के घर तक पहुँचाने का काम करता</p></summary> है। इसके माध्यम से आप अपने घर बैठे ही कई प्रकार की सरकारी सेवाओं का लाभ उठा सकते हैं।  चाहे वह आधार कार्ड, पैन कार्ड, या वोटर आईडी बनवाना हो, या फिर प्रधानमंत्री किसान सम्मान निधि, उज्ज्वला योजना, फसल बीमा योजना, मुद्रा लोन जैसी किसी भी सरकारी योजना के लिए आवेदन करना हो – समाधानी आपके लिए सब कुछ आसान बना देता है।
                         <h4>समाधानी के माध्यम से: </h4>
-                    </details>
+                    </details> */}
                     </div>
-                <div className='nav__left'><div className='left__main_nav'>
+                {/* <div className='nav__left'><div className='left__main_nav'>
                     <div className='parent-fdr-img'>
                         <div className='child-img'>
                             <div className='child-img-card'>
@@ -33,7 +33,7 @@ export default function header() {
                         </div>
                     </div>
                 </div>
-                </div>
+                </div> */}
             </div>
         </>
     )
